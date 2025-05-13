6eaf4c91f94845787a70baf95acc41395601e467
dirkluijk <mail@dirkluijk.nl>
2025-05-13 15:17:55 +0200

    style: apply consistent order

--- src/src/command.ts
+++ src/src/command.ts
@@ -8,14 +8,14 @@ export interface Command {
 export class MoveForwardCommand implements Command {
   execute(currentState: RoverState): RoverState {
     switch (currentState.direction) {
+      case Direction.NORTH:
+        return { ...currentState, y: currentState.y + 1 };
       case Direction.EAST:
         return { ...currentState, x: currentState.x + 1 };
       case Direction.SOUTH:
         return { ...currentState, y: currentState.y - 1 };
       case Direction.WEST:
         return { ...currentState, x: currentState.x - 1 };
-      case Direction.NORTH:
-        return { ...currentState, y: currentState.y + 1 };
     }
   }
 }
@@ -23,14 +23,14 @@ export class MoveForwardCommand implements Command {
 export class TurnLeftCommand implements Command {
   execute(currentState: RoverState): RoverState {
     switch (currentState.direction) {
-      case Direction.EAST:
-        return { ...currentState, direction: Direction.NORTH };
       case Direction.NORTH:
         return { ...currentState, direction: Direction.WEST };
-      case Direction.WEST:
-        return { ...currentState, direction: Direction.SOUTH };
+      case Direction.EAST:
+        return { ...currentState, direction: Direction.NORTH };
       case Direction.SOUTH:
         return { ...currentState, direction: Direction.EAST };
+      case Direction.WEST:
+        return { ...currentState, direction: Direction.SOUTH };
     }
   }
 }
@@ -38,14 +38,14 @@ export class TurnLeftCommand implements Command {
 export class TurnRightCommand implements Command {
   execute(currentState: RoverState): RoverState {
     switch (currentState.direction) {
+      case Direction.NORTH:
+        return { ...currentState, direction: Direction.EAST };
       case Direction.EAST:
         return { ...currentState, direction: Direction.SOUTH };
       case Direction.SOUTH:
         return { ...currentState, direction: Direction.WEST };
       case Direction.WEST:
         return { ...currentState, direction: Direction.NORTH };
-      case Direction.NORTH:
-        return { ...currentState, direction: Direction.EAST };
     }
   }
 }
